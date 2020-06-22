bc645fbb2e9e6b12c2109690ab2195df1d420958
Artur Balczynski <artur.balczynski@student.put.poznan.pl>
2020-06-22 02:15:03 +0200

    game starting protocole

--- src/main/tcp-client/webclient.pl
+++ src/main/tcp-client/webclient.pl
@@ -3,13 +3,59 @@
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/json)).
 
-connect_to_server() :- 
-	URL = 'ws://127.0.0.1:81/ws',
-	http_open_websocket(URL, WS, []),
-	ws_send(WS, json( _{
+open_websocket(URL, WebSocket) :- 
+	http_open_websocket(URL, WebSocket, []).
+
+connect_to_server(WebSocket) :- 
+	ws_send(WebSocket, json( _{
 		target: "global",
 		type: "rfx#jz",
 		zoneName: "morabaraba"
 	})),
-	ws_receive(WS, Reply, [ format(json) ]),
-	print(Reply.data.userData.name).
+	ws_receive(WebSocket, Reply, [ format(json) ]),
+	writeln(Reply.data).
+
+join_lobby(WebSocket) :- 
+	% ws_send(WebSocket, json( _{
+	% 	roomName: "lobby",
+	% 	target: "zone",
+	% 	type: "rfx#jr"
+	% })),
+	ws_send(WebSocket, text('{"roomName":"lobby", "target":"zone", "type":"rfx#jr"}')),
+	ws_receive(WebSocket, Reply, [ format(json) ]),
+	writeln(Reply.data).
+
+init_game(WebSocket) :- 
+	% ws_send(WebSocket, json( _{
+	% 	target: "zone",
+	% 	type: "si#gs"
+	% })),
+	ws_send(WebSocket, text('{"target":"zone","type":"si#gs"}')),
+	ws_receive(WebSocket, Reply, [ format(json) ]),
+	writeln(Reply.data).
+
+wait_for_game_start(WebSocket, PlayerColor) :- 
+	ws_receive(WebSocket, Reply, [ format(json) ]),
+	writeln(Reply.data),
+	( Reply.data.type == "si#gb" -> 
+		writeln(gameok),
+		writeln(Reply.data.isYourTurn),
+		writeln(PlayerColor),
+		is_your_turn_player_color(Reply.data.isYourTurn, PlayerColor)
+	;
+		writeln(nook),
+		wait_for_game_start(WebSocket, _)
+	).
+
+is_your_turn_player_color(true, white).
+is_your_turn_player_color(false, black).
+
+test() :- 
+	URL = "ws://127.0.0.1:81/ws",
+	open_websocket(URL, WebSocket),
+	connect_to_server(WebSocket),
+	join_lobby(WebSocket),
+	init_game(WebSocket),
+	wait_for_game_start(WebSocket, PlayerColor),
+	writeln(PlayerColor).
+
